3f0fc1e607ea057d668fc6f0e23a85f03e988344
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-03-09 17:31:25 -0300

    Firma del Token SEED OK

--- src/config/plugins/envs.ts
+++ src/config/plugins/envs.ts
@@ -13,6 +13,8 @@ export const envs = {
     MAILER_EMAIL : env.get('MAILER_EMAIL').required().asString(),
     MAILER_SECRET_KEY : env.get('MAILER_SECRET_KEY').required().asString(),
 
+    JWT_SEED : env.get('JWT_SEED').required().asString(),
+
     WEBSERVICE_URL : env.get('WEBSERVICE_URL').required().asString(),
 
 }
--- src/config/plugins/jwt.adapter.ts
+++ src/config/plugins/jwt.adapter.ts
@@ -1,4 +1,8 @@
 import jwt, { SignOptions } from 'jsonwebtoken';
+import { envs } from './envs';
+
+
+const JWT_SEED = envs.JWT_SEED;
 
 
 export class JwtAdapter {
@@ -6,7 +10,7 @@ export class JwtAdapter {
     static async generateToken(payload: any, duration: SignOptions['expiresIn'] = '2h') {
 
         return new Promise((resolve) => {
-            jwt.sign(payload, "SEED", { expiresIn: duration }, (err, token) => {
+            jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (err, token) => {
 
                 if (err) return resolve(null);
                 return resolve(token);
