6e4cd0cad55f985bbba8e22ead30f93ded0babce
jaavispeed <ja.rodriguezc@duocuc.cl>
2025-03-12 17:54:40 -0300

    cors nose

--- src/config/cors.ts
+++ src/config/cors.ts
@@ -5,7 +5,7 @@ import { envs } from "./plugins/envs";
 // Configuración de CORS
 export const corsConfig = cors({
   origin: envs.CLIENT_URL, // Origen permitido (puedes usar una variable de entorno)
-  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Métodos permitidos
-  credentials: true, // Si necesitas enviar cookies o autenticación
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Métodos permitidos
+  credentials: false, // Si necesitas enviar cookies o autenticación
   allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-TOKEN'],
 });
